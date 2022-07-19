7765462368cc73e660d011e55c069aebabac1162
Pamela Rondina <pamela.rondina@gmail.com>
2022-07-19 19:13:58 -0300

    ok

--- Aula 01/src/App.java
+++ Aula 01/src/App.java
@@ -3,13 +3,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // fazer uma conexão HTTP e buscar os top 250 filmes
-        // String url = "https://imdb-api.com/en/API/Top250Movies/k_0tcwmpdk";
         String url = "https://alura-filmes.herokuapp.com/conteudos";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -19,17 +16,8 @@ public class App {
         System.out.println(body);
 
         // extrair só os dados que interessem (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
         // exibir e manipular os dados
-        for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
-            System.out.println();
-            
 
-        }
     }
 }
